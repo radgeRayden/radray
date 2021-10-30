58f3017987739ecfe1be7128aa84e377ad778e75
Westerbly Snaydley <westerbly@gmail.com>
2021-10-29 20:48:45 -0500

    use helper to convert from normalized color to bytes

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,21 @@ struct Color {
     b: u8,
 }
 
+impl Color {
+    fn from_normalized(r: f32, g: f32, b: f32) -> Color {
+        Color {
+            r: (r * 255.0).round() as u8,
+            g: (g * 255.0).round() as u8,
+            b: (b * 255.0).round() as u8
+        }
+    }
+}
+
 fn color(x: f32, y: f32) -> Color {
-    let r = (x * 255.0) as u8;
-    let g = (y * 255.0) as u8;
-    let b = (x * y * 255.0) as u8;
-    Color {r, g, b}
+    let r = x;
+    let g = y;
+    let b = x * y;
+    Color::from_normalized(r, g, b)
 }
 
 fn main() -> std::io::Result<()> {
